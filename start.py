6d21210b2710e988599a6bb538b053872d0cc407
nikcadez <nikcadez@gmail.com>
2021-05-23 02:40:32 +0200

    scraper.py commit

---
+++ start.py
@@ -0,0 +1,58 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+
+class start:
+
+    def __init__(self, name, lon, lat, ok): # name = name of the awesome paraStart
+        self.name = name
+        self.data = []
+        self.lon = lon
+        self.lat = lat
+        self.ok = ok
+
+        # scraping magic
+        soup = BeautifulSoup(requests.post("http://skytech.si/skytechsys/data.php", data={"c": "tabela"}).text, features="html.parser").get_text()
+        for i in range(1, 6):
+            # extract a piece of data
+            piece_of_data = re.findall(name+"\n(.*\n){"+str(i)+"}", string=soup)[0].strip() 
+            # add the piece of data to list
+            # data = [wind speed, wind bust, wind direction, temperature, time and date]
+            self.data.append(piece_of_data) 
+    
+    # get current wind speed
+    def getWindSpeed(self):
+        return self.data[0]
+    
+    # get current wind bust
+    def getWindBust(self):
+        return self.data[1]
+    
+    # get current wind direction
+    def getWindDirection(self):
+        return self.data[2]
+    
+    # get current wind temperature
+    def getTemperature(self):
+        return self.data[3]
+    
+    # get current wind temperature
+    def getTimeAndDate(self):
+        return self.data[4]
+    
+    # get Longitude
+    def getLongitude(self):
+        return self.lon
+    
+    # get Latitude
+    def getLatitude(self):
+        return self.lat
+    
+    # get optimal wind speeds
+    def getGoodWind(self):
+        return self.ok
+
+    # returns true if wind is optimal, else false
+    def isWindGood(self):
+        return (self.data[2] in self.ok)
---
+++ test.py
@@ -0,0 +1,8 @@
+def generirajLink(lat,long):
+    coord_API_endpoint = "http://api.openweathermap.org/data/2.5/weather?"
+    lat_long = "lat=" + str(lat) + "&lon=" + str(long)
+    join_key = "&appid=" + "fccbb7f106f4603371910d9b192f519e" # drugi je kljuc
+    units = "&units=metric"
+    current_coord_weather_url = coord_API_endpoint + lat_long + join_key + units
+    return current_coord_weather_url
+print(generirajLink(46.33941534887242, 14.331205906510517))
