7c12ce2abbda419febdecaa4756edd80204172df
Žiga Premrl <ziga.premrl1@gmail.com>
2021-07-06 11:21:25 +0200

    Started fixing api

--- app.py
+++ app.py
@@ -1,37 +1,173 @@
 import json
-import start
+import server.apiUtil as start
 from flask import Flask
 from flask import request
 
 app = Flask(__name__)
 
-# returns json of names + coordinats
+# returns json of names + coordinates
+
+# hardcoded list of sites, could get list from scraping
+sites = {
+    "Gozd": {
+        "lon": 46.3395,
+        "lat": 14.3313,
+        "ok": {"JZ", "J", "JV"}
+    },
+    "Ratitovec": {
+        "lon": 46.2361,
+        "lat": 14.0906,
+        "ok": {"J", "JV"}
+    },
+    "Vogar": {
+        "lon": 46.2946,
+        "lat": 13.8755,
+        "ok": {"JZ", "J", "JV"}
+    },
+    "Vogel": {
+        "lon": 46.2518,
+        "lat": 13.839,
+        "ok": {"JV", "V", "SV"}
+    },
+    "Kranjska Gora": {
+        "lon": 46.5044,
+        "lat": 13.7954,
+        "ok": {"J"}
+    },
+    "Ambrož pod Krvavcem": {
+        "lon": 46.2752,
+        "lat": 14.5279,
+        "ok": {"JZ", "J", "JV"}
+    },
+    "Kriška gora": {
+        "lon": 46.3515,
+        "lat": 14.3332,
+        "ok": {"JZ", "J", "JV"}
+    },
+    "Velika planina": {
+        "lon": 46.2946,
+        "lat": 14.6395,
+        "ok": {"J", "JV"}
+    },
+    "Mangrtsko Sedlo": {
+        "lon": 46.4334,
+        "lat": 13.6407,
+        "ok": {"JZ", "J", "JV", "V", "Z"}
+    },
+    "Kobala": {
+        "lon": 46.1806,
+        "lat": 13.7791,
+        "ok": {"JZ", "J", "JV", "V", "Z"}
+    },
+    "Kovk": {
+        "lon": 45.8865,
+        "lat": 13.9591,
+        "ok": {"JZ", "J", "JV"}
+    },
+    "Kobariški Stol": {
+        "lon": 46.2727,
+        "lat": 13.4732,
+        "ok": {"JZ", "J", "JV", "V", "Z"}
+    },
+    "Srednji vrh - Matajur": {
+        "lon": 46.209,
+        "lat": 13.5663,
+        "ok": {"SV", "S"}
+    },
+    "Kobariški Kuk - jug": {
+        "lon": 46.1952,
+        "lat": 13.6198,
+        "ok": {"JZ", "J", "JV"}
+    },
+    "Lijak": {
+        "lon": 45.9636,
+        "lat": 13.7236,
+        "ok": {"JZ", "J", "JV", "V", "Z"}
+    },
+    "Slivnica": {
+        "lon": 45.7886,
+        "lat": 14.4067,
+        "ok": {"JZ", "J", "JV", "Z"}
+    },
+    "Slivnica": {
+        "lon": 45.7886,
+        "lat": 14.4067,
+        "ok": {"JZ", "J", "JV", "Z"}
+    },
+    "Kamšak": {
+        "lon": 46.3579,
+        "lat": 15.259,
+        "ok": {"JZ", "J"}
+    },
+    "Konjiška gora": {
+        "lon": 46.3347,
+        "lat": 15.3466,
+        "ok": {"JZ", "J"}
+    },
+    "Mala Gora": {
+        "lon": 46.3574,
+        "lat": 15.3397,
+        "ok": {"JZ", "V", "SV"}
+    },
+    "Malič": {
+        "lon": 46.1822,
+        "lat": 15.2056,
+        "ok": {"J", "JV"}
+    },
+    "Donačka gora": {
+        "lon": 46.2616,
+        "lat": 15.7313,
+        "ok": {"JZ", "J", "JV"}
+    },
+    "Žusem": {
+        "lon": 46.1519,
+        "lat": 15.4909,
+        "ok": {"V", "SV", "S"}
+    },
+    "Pohorje": {
+        "lon": 46.5164,
+        "lat": 15.58,
+        "ok": {"SV", "S", "SZ"}
+    },
+    "Golte": {
+        "lon": 46.3705,
+        "lat": 14.9206,
+        "ok": {"J", "JV", "V"}
+    },
+    "Golte": {
+        "lon": 46.3705,
+        "lat": 14.9206,
+        "ok": {"J", "JV", "V"}
+    }
+}
 
 
-@app.route('/list')
+@app.route("/list")
 def getList():
-    list = {'jumpPoints': []}
-    for i in sites:
-        pom = sites.get(i)
-        list.get('jumpPoints').append(
-            {i: {'lon': pom.get('lon'), 'lan': pom.get('lan')}})
-    return json.dumps(list)
+    list = {}
+    for name in sites.keys():
+        filtered = {}
+        filtered["lat"] = sites[name].get("lat")
+        filtered["lon"] = sites[name].get("lon")
+        list[name] = filtered
+    print(list.keys())
+    return list  # flask converts dict to json
 
 # returns json of data for given name + coordinats
 
 
-@app.route('/data', methods=['GET', 'POST'])
+@ app.route("/data", methods=["GET", "POST"])
 def getData():
     name = request.args["name"]
     atributes = sites.get(name)
-    x = atributes.get('lon')
-    y = atributes.get('lat')
-    ok = atributes.get('ok')
+    x = atributes.get("lon")
+    y = atributes.get("lat")
+    ok = atributes.get("ok")
     t = start(name, x, y, ok)
     jumpPoint = {
         "WindSpeed": t.getWindSpeed,  # double
         "WindBust": t.getWindBust(),  # double
-        "WindDirection": t.getWindDirection(),  # chat or str
+        "WindDirection": t.getWindDirection(),  # char or str
         "Temperature": t.getTemperature(),  # double
         "TimeAndDate": t.getTimeAndDate(),  # date
         "isWindGood": t.isWindGood()  # T/F
@@ -40,143 +176,9 @@ def getData():
 
 
 # run() - runs the application on the local development server.
-#app.run([host, port, debug, options])
+# app.run([host, port, debug, options])
 # host - Hostname to listen on. Defaults to 127.0.0.1 (localhost).
 # port - defaults to 5000
-if __name__ == '__main__':
-    app.run()
-
-
-# hardcoded list of sites, could get list from scraping
-sites = {
-    'Gozd': {
-        'lon': 46.3395,
-        'lan': 14.3313,
-        'ok': {'JZ', 'J', 'JV'}
-    },
-    'Ratitovec': {
-        'lon': 46.2361,
-        'lan': 14.0906,
-        'ok': {'J', 'JV'}
-    },
-    'Vogar': {
-        'lon': 46.2946,
-        'lan': 13.8755,
-        'ok': {'JZ', 'J', 'JV'}
-    },
-    'Vogel': {
-        'lon': 46.2518,
-        'lan': 13.839,
-        'ok': {'JV', 'V', 'SV'}
-    },
-    'Kranjska Gora': {
-        'lon': 46.5044,
-        'lan': 13.7954,
-        'ok': {'J'}
-    },
-    'Ambrož pod Krvavcem': {
-        'lon': 46.2752,
-        'lan': 14.5279,
-        'ok': {'JZ', 'J', 'JV'}
-    },
-    'Kriška gora': {
-        'lon': 46.3515,
-        'lan': 14.3332,
-        'ok': {'JZ', 'J', 'JV'}
-    },
-    'Velika planina': {
-        'lon': 46.2946,
-        'lan': 14.6395,
-        'ok': {'J', 'JV'}
-    },
-    'Mangrtsko Sedlo': {
-        'lon': 46.4334,
-        'lan': 13.6407,
-        'ok': {'JZ', 'J', 'JV', 'V', 'Z'}
-    },
-    'Kobala': {
-        'lon': 46.1806,
-        'lan': 13.7791,
-        'ok': {'JZ', 'J', 'JV', 'V', 'Z'}
-    },
-    'Kovk': {
-        'lon': 45.8865,
-        'lan': 13.9591,
-        'ok': {'JZ', 'J', 'JV'}
-    },
-    'Kobariški Stol': {
-        'lon': 46.2727,
-        'lan': 13.4732,
-        'ok': {'JZ', 'J', 'JV', 'V', 'Z'}
-    },
-    'Srednji vrh - Matajur': {
-        'lon': 46.209,
-        'lan': 13.5663,
-        'ok': {'SV', 'S'}
-    },
-    'Kobariški Kuk - jug': {
-        'lon': 46.1952,
-        'lan': 13.6198,
-        'ok': {'JZ', 'J', 'JV'}
-    },
-    'Lijak': {
-        'lon': 45.9636,
-        'lan': 13.7236,
-        'ok': {'JZ', 'J', 'JV', 'V', 'Z'}
-    },
-    'Slivnica': {
-        'lon': 45.7886,
-        'lan': 14.4067,
-        'ok': {'JZ', 'J', 'JV', 'Z'}
-    },
-    'Slivnica': {
-        'lon': 45.7886,
-        'lan': 14.4067,
-        'ok': {'JZ', 'J', 'JV', 'Z'}
-    },
-    'Kamšak': {
-        'lon': 46.3579,
-        'lan': 15.259,
-        'ok': {'JZ', 'J'}
-    },
-    'Konjiška gora': {
-        'lon': 46.3347,
-        'lan': 15.3466,
-        'ok': {'JZ', 'J'}
-    },
-    'Mala Gora': {
-        'lon': 46.3574,
-        'lan': 15.3397,
-        'ok': {'JZ', 'V', 'SV'}
-    },
-    'Malič': {
-        'lon': 46.1822,
-        'lan': 15.2056,
-        'ok': {'J', 'JV'}
-    },
-    'Donačka gora': {
-        'lon': 46.2616,
-        'lan': 15.7313,
-        'ok': {'JZ', 'J', 'JV'}
-    },
-    'Žusem': {
-        'lon': 46.1519,
-        'lan': 15.4909,
-        'ok': {'V', 'SV', 'S'}
-    },
-    'Pohorje': {
-        'lon': 46.5164,
-        'lan': 15.58,
-        'ok': {'SV', 'S', 'SZ'}
-    },
-    'Golte': {
-        'lon': 46.3705,
-        'lan': 14.9206,
-        'ok': {'J', 'JV', 'V'}
-    },
-    'Golte': {
-        'lon': 46.3705,
-        'lan': 14.9206,
-        'ok': {'J', 'JV', 'V'}
-    }
-}
+# debug = True for developement enviorment
+if __name__ == "__main__":
+    app.run(debug=True)
--- server/apiUtil.py
+++ server/apiUtil.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 class start:
 
-    def __init__(self, name, lon, lat, ok):  # name = name of the awesome paraStart
+    def __init__(self, name, lon, lat, ok):
         self.name = name
         self.data = []
         self.lon = lon
@@ -13,23 +13,20 @@ class start:
         self.ok = ok
 
         # scraping magic
-        # TODO: fix, currently i think that 0.0 speeds are being sent as an empty line
         soup = BeautifulSoup(requests.post("http://skytech.si/skytechsys/data.php",
                              data={"c": "tabela"}).text, features="html.parser").get_text()
+        # go through data piece by piece
         for i in range(1, 6):
-            # extract a piece of data
             piece_of_data = re.findall(
                 name+"\n(.*\n){"+str(i)+"}", string=soup)[0].strip()
-            # and add it to list
-            # data = [wind speed, wind bust, wind direction, temperature, time and date]
             self.data.append(piece_of_data)
 
     # get current wind speed
     def getWindSpeed(self):
         return self.data[0]
 
-    # get current wind bust
-    def getWindBust(self):
+    # get current wind gust
+    def getWindGust(self):
         return self.data[1]
 
     # get current wind direction
